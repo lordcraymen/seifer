229b9f8de03871896bd695ac401faee9afc8062e
Florian Patzke <florian.patzke@t-online.de>
2025-02-09 12:43:17 +0100

    Korrigiere Importpfad für die Funktion getPropertyMap in den Tests

--- test/Entities/PropertyMap/parsePropertyMap.test.ts
+++ test/Entities/PropertyMap/parsePropertyMap.test.ts
@@ -1,4 +1,4 @@
-import { getPropertyMap } from "../../src/Entities/PropertyMap";
+import { getPropertyMap } from "../../../src/Entities/PropertyMap/getPropertyMap";
 
 describe("getPropertyMap", () => {
   it("should parse a property map string with sloppy quotes ", () => {
