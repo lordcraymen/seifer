12245449892a46b71d749e5574f15205353d62a1
Florian Patzke <florian.patzke@t-online.de>
2025-02-10 09:32:20 +0100

    Füge Funktion toNormalCase hinzu, um Befehle in Proper Case zu formatieren und entferne leere Abfragen in getCypherQuery

--- src/Entities/Clauses/getClauses.ts
+++ src/Entities/Clauses/getClauses.ts
@@ -1,12 +1,18 @@
 import { Clause } from "../../types";
 import { _parseClauses, ProtoClause } from "./_parseClauses";
 
+const toNormalCase = (str: string): string => {
+    //make the words formatted line proper nouns with capital letter at the beginning
+    return str.toLowerCase().split(' ').map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+}
+
 const getClauses = (query:string): Clause[] => {
     const protoClauses: ProtoClause[] = _parseClauses(query);
     return protoClauses.map((pc) => {
         return {
-            type: `${pc.command}Clause`,
-            command: pc.command,
+            type: `${toNormalCase(pc.command)}Clause`,
             content: pc.content,
         };
     });
--- src/Entities/CypherQuery/getCypherQuery.ts
+++ src/Entities/CypherQuery/getCypherQuery.ts
@@ -3,7 +3,6 @@ import { _validateCypherQuery } from "./_validateCypherQuery";
 import { getClauses } from "../Clauses";
 
 const getCypherQuery = (query: string): CypherQuery => {
-  if (query.trim() === "") return { type: "Query", clauses: [] };
   return _validateCypherQuery({
     type: "Query",
     clauses: getClauses(query),
